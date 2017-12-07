ae6371d26f7edb89c43efff06f87abfd2d477629
javademami <aliabadi7697@gmail.com>
2017-12-07 20:16:13 +0330

    First commit

---
+++ cmake-build-debug/CMakeFiles/DS_DNA_list.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DS_DNA_list.dir/main.cpp.o"
+  "DS_DNA_list.pdb"
+  "DS_DNA_list"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DS_DNA_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
